d574f3c56f662a746d3062bd39fabf120fe2b40a
wnstjr <wnstjr8717@gmail.com>
2021-09-27 21:00:50 +0900

    connect fastapi.websocket

--- socket_client.py
+++ socket_client.py
@@ -6,7 +6,7 @@ import json
 
 async def conn(main):
     # "ws://4.tcp.ngrok.io:12958" tcp 주소 예
-    async with websockets.connect("ws://127.0.0.1:9998") as websocket:
+    async with websockets.connect("ws://127.0.0.1:8000/ws") as websocket:
         while True:
             await websocket.send(json.dumps(main.client))
             if main.client['POST']:
