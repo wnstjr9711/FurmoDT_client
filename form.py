fef4767e7a80fb0eeecd52c3bbe4eeee3ae3ad90
wnstjr <wnstjr8717@gmail.com>
2021-10-01 11:33:26 +0900

    자막 파일 drag and drop

--- form.py
+++ form.py
@@ -9,6 +9,22 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
         self.main = AdvancedSetup(self)
+        self.setAcceptDrops(True)
+
+    def dragEnterEvent(self, e):
+        e.accept()
+
+    def dragMoveEvent(self, e):
+        target = self.main.work_widget
+        x_range = range(*(target.x(), target.x() + target.width() + 1))
+        y_range = range(*(target.y(), target.y() + target.height() + 1))
+        if e.pos().x() in x_range and e.pos().y() in y_range:
+            e.accept()
+        else:
+            e.ignore()
+
+    def dropEvent(self, e):
+        self.main.add_subtitle(e.mimeData().urls()[0].toLocalFile())
 
 
 app = QApplication(sys.argv)
